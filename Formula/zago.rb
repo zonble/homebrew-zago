a7de7621ffafc30770ff7030387e4200eba6bbed
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-08-09 02:59:46 +0800

    zago 1.2.1

--- Formula/zago.rb
+++ Formula/zago.rb
@@ -1,8 +1,8 @@
 class Zago < Formula
   desc "Terminal text editor and plain-text diagramming tool"
   homepage "https://github.com/zonble/zago"
-  url "https://github.com/zonble/zago/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "d7c5cae57b5d1fa05cd0adde5bbd2a10da42728445f5a3b862a4b6a6f7822ee3"
+  url "https://github.com/zonble/zago/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "1ddde5190e336b73d02b38eb3288e83c99e4498b3b642ce1944e698686edd886"
   license "MIT"
   head "https://github.com/zonble/zago.git", branch: "main"
 
